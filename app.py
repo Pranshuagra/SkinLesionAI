ce905ed5b7e4d479f5ffad01cab9d4bfd4f04c49
Pranshu Agrahari <pranshuagrahari11.com>
2026-05-30 05:12:42 +0530

    Prepare project for Hugging Face deployment

--- app.py
+++ app.py
@@ -258,8 +258,17 @@ def batch_predict():
 # RUN APP
 # ==================================================
 
+import os
+
 if __name__ == "__main__":
+    port = int(
+        os.environ.get(
+            "PORT",
+            7860
+        )
+    )
 
     app.run(
-        debug=True
+        host="0.0.0.0",
+        port=port
     )
